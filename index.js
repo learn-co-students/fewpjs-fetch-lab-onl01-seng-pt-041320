f46281790866e49d0f27b7cadd50273f6ccdb7d7
marcusLau <marcus.lau76@gmail.com>
2020-10-30 16:35:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -11,6 +13,6 @@ function renderBooks(books) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks()
 })
